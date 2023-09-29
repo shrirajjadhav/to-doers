a9be9bc1b52b1e5aaf4cc30e02d8b76b37a137d6
Shriraj Jadhav <git@shriraj.dev>
2023-09-29 21:32:47 +0530

    Changed app name - To-Doers
    - added new app name to page title, header.
    - added repo link in footer.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@
 const Header = () => {
   return (
     <header>
-        <h1 className=' text-5xl font-bold mx-4 mt-8' >Task Tracker</h1>
-        <p className=' text-xl mx-4 my-8'>A simple Task Tracker 😎</p>
+        <h1 className=' text-5xl font-bold mx-4 mt-8' >To-Doers</h1>
+        <p className=' text-xl mx-4 my-8'>A simple ToDo app for the people who do!!! 😎</p>
     </header>
   )
 }
